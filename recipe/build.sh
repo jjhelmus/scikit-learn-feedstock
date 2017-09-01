63dfb942179bd12b740edcd5d6c0bcd9ffa97c3e
Michael Sarahan <msarahan@gmail.com>
2017-09-01 08:44:31 -0500

    mkl and cb3

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-if [ `uname` == "Darwin" ]
-then
-    export LDFLAGS="-headerpad_max_install_names"
+if [ "$blas_impl" = "mkl" ]; then
+
+    cat > site.cfg <<EOF
+[mkl]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+mkl_libs = mkl_rt
+lapack_libs =
+
+EOF
+
+else # Non-MKL
+
+    cat > site.cfg <<EOF
+[DEFAULT]
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+
+[atlas]
+atlas_libs = openblas
+libraries = openblas
+
+[openblas]
+libraries = openblas
+library_dirs = $PREFIX/lib
+include_dirs = $PREFIX/include
+
+EOF
+
 fi
 
 "${PYTHON}" setup.py install --single-version-externally-managed --record record.txt
